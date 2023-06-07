cf540ad4ba7208b6e9ae47d34e9d56bb166dfe3b
Darío Salazar Leal <dario12_58@hotmail.com>
2023-06-07 13:46:32 -0600

    feat: delete and change state of task

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -4,27 +4,33 @@ import { Tarea } from 'src/app/models/Tareas';
 @Component({
   selector: 'app-tareas',
   templateUrl: './tareas.component.html',
-  styleUrls: ['./tareas.component.css']
+  styleUrls: ['./tareas.component.css'],
 })
-export class TareasComponent implements OnInit{
-  listTareas: Tarea[] = []
+export class TareasComponent implements OnInit {
+  listTareas: Tarea[] = [];
   nombreTarea = '';
 
   constructor() {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   agregarTarea() {
     //Crear un objeto tarea
     const tarea: Tarea = {
       nombre: this.nombreTarea,
-      estado: false
-    }
+      estado: false,
+    };
     //Agregar el objeto tarea al array;
     this.listTareas.push(tarea);
     //Reset form
     this.nombreTarea = '';
+  }
+
+  eliminarTarea(index: number): void {
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
   }
 }
